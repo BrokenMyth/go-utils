d3fec2d58af36a745780a2fe35eed27f4c28804c
qx.jiang <qx.jiang@aftership.com>
2023-12-28 16:02:47 +0800

    fix: some bug

--- shell/go/install_go.sh
+++ shell/go/install_go.sh
@@ -3,6 +3,6 @@
 wget https://golang.org/dl/go1.21.5.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.21.5.linux-amd64.tar.gz
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.zshrc
-echo 'export PATH=$PATH:$GOPATH/bin' >> ~/.zshrc
+echo 'export PATH="$PATH:$(go env GOPATH)/bin"' >> ~/.zshrc
 . ~/.zshrc
 go version
--- shell/go/uninstall_go.sh
+++ shell/go/uninstall_go.sh
@@ -2,5 +2,5 @@
 
 rm -rf /usr/local/go
 sed -i '/export PATH=\$PATH:\/usr\/local\/go\/bin/d' ~/.zshrc
-sed -i '/export PATH=\$PATH:\$GOPATH\/bin/d' ~/.zshrc
+sed -i '/export PATH="$PATH:$(go env GOPATH)\/bin"/d' ~/.zshrc
 . ~/.zshrc
