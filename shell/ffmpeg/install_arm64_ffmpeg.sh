b1166b5ce4b08f2d16a94a24651b5f1599561154
qx.jiang <qx.jiang@aftership.com>
2023-12-28 15:28:23 +0800

    feat: add shell readme

---
+++ shell/ffmpeg/install_arm64_ffmpeg.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#x86_64下载二进制文件
+wget https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-arm64-static.tar.xz
+
+#解压文件
+tar xvf ffmpeg-git-*-static.tar.xz && rm -rf ffmpeg-git-*-static.tar.xz
+
+#将ffmpeg和ffprobe可执行文件移至/usr/bin方便系统直接调用
+mv ffmpeg-git-*/ffmpeg  ffmpeg-git-*/ffprobe /usr/bin/
+
+#查看版本
+ffmpeg -version
