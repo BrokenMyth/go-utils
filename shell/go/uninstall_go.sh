0971676e01ca443cf07e256666103d443386e461
qx.jiang <qx.jiang@aftership.com>
2023-12-28 16:36:26 +0800

    feat: change source

--- shell/go/uninstall_go.sh
+++ shell/go/uninstall_go.sh
@@ -3,4 +3,4 @@
 rm -rf /usr/local/go
 sed -i '/export PATH=\$PATH:\/usr\/local\/go\/bin/d' ~/.zshrc
 sed -i '/export PATH="$PATH:$(go env GOPATH)\/bin"/d' ~/.zshrc
-zsh -i -c 'source ~/.zshrc'
+source ~/.zshrc
