0dc6a75d2288892213bb2bc8971783430f20b774
qx.jiang <qx.jiang@aftership.com>
2024-01-02 14:39:22 +0800

    feat: change zshrc to bashrc

--- shell/go/install_go.sh
+++ shell/go/install_go.sh
@@ -2,7 +2,7 @@
 
 wget https://golang.org/dl/go1.21.5.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.21.5.linux-amd64.tar.gz
-echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.zshrc
-echo 'export PATH="$PATH:$(go env GOPATH)/bin"' >> ~/.zshrc
-source ~/.zshrc
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
+echo 'export PATH="$PATH:$(go env GOPATH)/bin"' >> ~/.bashrc
+source ~/.bashrc
 go version
--- shell/go/uninstall_go.sh
+++ shell/go/uninstall_go.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rm -rf /usr/local/go
-sed -i '/export PATH=\$PATH:\/usr\/local\/go\/bin/d' ~/.zshrc
-sed -i '/export PATH="$PATH:$(go env GOPATH)\/bin"/d' ~/.zshrc
-source ~/.zshrc
+sed -i '/export PATH=\$PATH:\/usr\/local\/go\/bin/d' ~/.bashrc
+sed -i '/export PATH="$PATH:$(go env GOPATH)\/bin"/d' ~/.bashrc
+source ~/.bashrc
